e0f3a1c1710257f247ba13c5727caad49f9d585a
sergiylontkovsky <ls1>
2018-03-06 19:55:14 +0200

    bitcoin

---
+++ js/config.js
@@ -0,0 +1,24 @@
+config = {
+
+    // Translation files
+    i18nFilesLocation: 'i18n/',
+
+    // API locations
+    api: {
+        local : 'http://localhost:8000/',
+        test : 'http://devapi01.secureleadsnow.com/',
+        live : 'https://api.trackinglove.com/',
+    },
+
+    // Phone number field settings
+    preferredAreaCodes: ["us", "gb", "au", "ca"],
+
+    // Funnel config
+    funnel: 'bitcointrader',
+
+    // Assign random version for A/B testing
+    version: 'A',
+
+    loadStatic: ['nl', 'it']
+
+}
